6b4d79a2e71372ae09efd8c0ea52bbfd1c362395
Andrew Fernandez <andrew.fernandez@caci.com>
2023-08-23 15:59:51 -0400

    Spring Configuration for needed Operator Beans

---
+++ src/main/java/af/ex/kube/kubeoperator/config/OperatorConfig.java
@@ -0,0 +1,20 @@
+package af.ex.kube.kubeoperator.config;
+
+import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OperatorConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    @SuppressWarnings("rawtypes")
+    public Operator operator(List<Reconciler> controllers) {
+        Operator operator = new Operator();
+        controllers.forEach(operator::register);
+        return operator;
+    }
+}
