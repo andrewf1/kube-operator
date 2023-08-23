b5d0f50c66be59a6afd032b2fba7f392a142c218
Andrew Fernandez <andrew.fernandez@caci.com>
2023-08-23 15:01:23 -0400

    Inital commit with Spring Initializr

---
+++ src/main/java/af/ex/kube/kubeoperator/KubeOperatorApplication.java
@@ -0,0 +1,12 @@
+package af.ex.kube.kubeoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeOperatorApplication.class, args);
+	}
+}
---
+++ src/test/java/af/ex/kube/kubeoperator/KubeOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package af.ex.kube.kubeoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
