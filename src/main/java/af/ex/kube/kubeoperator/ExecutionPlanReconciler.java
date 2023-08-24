46774ba13f43d5d09b2898143c54d4f4416d0f43
Andrew Fernandez <fdez26@ymail.com>
2023-08-24 16:29:53 -0400

    Adds event listener for Deployment resources

--- src/main/java/af/ex/kube/kubeoperator/ExecutionPlanReconciler.java
+++ src/main/java/af/ex/kube/kubeoperator/ExecutionPlanReconciler.java
@@ -1,10 +1,21 @@
 package af.ex.kube.kubeoperator;
 
 import af.ex.kube.kubeoperator.resource.custom.ExecutionPlan;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.processing.event.source.EventSource;
+import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
+import io.javaoperatorsdk.operator.processing.event.source.informer.Mappers;
+
+import java.util.List;
+import java.util.Map;
 
 @ControllerConfiguration
-public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Cleaner<ExecutionPlan> {
+public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Cleaner<ExecutionPlan>,
+        EventSourceInitializer<ExecutionPlan> {
+
+    public static final String SELECTOR = "";
 
     @Override
     public UpdateControl<ExecutionPlan> reconcile(ExecutionPlan resource,
@@ -15,5 +26,14 @@ public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Clean
     @Override
     public DeleteControl cleanup(ExecutionPlan resource, Context<ExecutionPlan> context) {
         return DeleteControl.defaultDelete();
+    }
+
+    @Override
+    public Map<String, EventSource> prepareEventSources(EventSourceContext<ExecutionPlan> context) {
+        var deploymentEventSource = new InformerEventSource<>(InformerConfiguration.from(Deployment.class, context)
+                .withLabelSelector(SELECTOR)
+                .build(), context);
+
+        return EventSourceInitializer.nameEventSources(deploymentEventSource);
     }
 }
