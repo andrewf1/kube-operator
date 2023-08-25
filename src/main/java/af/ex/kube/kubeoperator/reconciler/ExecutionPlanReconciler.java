3e00358714c19bfc22e48e99b67d08567263add3
Andrew Fernandez <andrew.fernandez@caci.com>
2023-08-25 13:25:31 -0400

    Re-factors package structure

--- src/main/java/af/ex/kube/kubeoperator/reconciler/ExecutionPlanReconciler.java
+++ src/main/java/af/ex/kube/kubeoperator/reconciler/ExecutionPlanReconciler.java
@@ -1,4 +1,4 @@
-package af.ex.kube.kubeoperator;
+package af.ex.kube.kubeoperator.reconciler;
 
 import af.ex.kube.kubeoperator.resource.custom.ExecutionPlan;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
@@ -6,14 +6,12 @@ import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
-import io.javaoperatorsdk.operator.processing.event.source.informer.Mappers;
 
-import java.util.List;
 import java.util.Map;
 
 @ControllerConfiguration
 public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Cleaner<ExecutionPlan>,
-        EventSourceInitializer<ExecutionPlan> {
+        EventSourceInitializer<ExecutionPlan>, ErrorStatusHandler<ExecutionPlan> {
 
     public static final String SELECTOR = "";
 
@@ -35,5 +33,12 @@ public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Clean
                 .build(), context);
 
         return EventSourceInitializer.nameEventSources(deploymentEventSource);
+    }
+
+    @Override
+    public ErrorStatusUpdateControl<ExecutionPlan> updateErrorStatus(ExecutionPlan resource,
+                                                                     Context<ExecutionPlan> context,
+                                                                     Exception e) {
+        return ErrorStatusUpdateControl.updateStatus(resource);
     }
 }
