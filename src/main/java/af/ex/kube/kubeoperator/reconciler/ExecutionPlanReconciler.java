c3e4eb17406414fabe2b85e98b7a7e04b4ba5039
Andrew Fernandez <andrew.fernandez@caci.com>
2023-08-25 16:54:50 -0400

    Begins working on the PrimaryToSecondaryMapper to associate the ExecutionPlan with the multiple Deployment resources

--- src/main/java/af/ex/kube/kubeoperator/reconciler/ExecutionPlanReconciler.java
+++ src/main/java/af/ex/kube/kubeoperator/reconciler/ExecutionPlanReconciler.java
@@ -1,13 +1,16 @@
 package af.ex.kube.kubeoperator.reconciler;
 
 import af.ex.kube.kubeoperator.resource.custom.ExecutionPlan;
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
+import io.javaoperatorsdk.operator.processing.event.source.PrimaryToSecondaryMapper;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
 
 import java.util.Map;
+import java.util.Set;
 
 @ControllerConfiguration
 public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Cleaner<ExecutionPlan>,
@@ -30,10 +33,13 @@ public class ExecutionPlanReconciler implements Reconciler<ExecutionPlan>, Clean
     public Map<String, EventSource> prepareEventSources(EventSourceContext<ExecutionPlan> context) {
         var deploymentEventSource = new InformerEventSource<>(InformerConfiguration.from(Deployment.class, context)
                 .withLabelSelector(SELECTOR)
+                .withPrimaryToSecondaryMapper((PrimaryToSecondaryMapper<? extends HasMetadata>) primary ->
+                    Set.of()
+                )
                 .build(), context);
-
         return EventSourceInitializer.nameEventSources(deploymentEventSource);
     }
+
 
     @Override
     public ErrorStatusUpdateControl<ExecutionPlan> updateErrorStatus(ExecutionPlan resource,
