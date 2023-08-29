24ea56a57aa38f0c8e7fc19347d5e072bdb66886
Andrew Fernandez <fdez26@ymail.com>
2023-08-29 19:15:39 -0400

    Removes unused Reconciler

--- src/main/java/af/ex/kube/kubeoperator/config/OperatorConfig.java
+++ src/main/java/af/ex/kube/kubeoperator/config/OperatorConfig.java
@@ -1,15 +1,8 @@
 package af.ex.kube.kubeoperator.config;
 
-import af.ex.kube.kubeoperator.reconciler.ExecutionPlanDependentWorkflowReconciler;
 import af.ex.kube.kubeoperator.reconciler.ExecutionPlanReconciler;
-import af.ex.kube.kubeoperator.resource.custom.ExecutionPlan;
-import af.ex.kube.kubeoperator.resource.dependent.DeploymentDependentResource;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResourceConfig;
-import io.javaoperatorsdk.operator.processing.dependent.workflow.Workflow;
-import io.javaoperatorsdk.operator.processing.dependent.workflow.WorkflowBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,19 +14,6 @@ public class OperatorConfig {
     @Bean
     public ExecutionPlanReconciler reconciler() {
         return new ExecutionPlanReconciler();
-    }
-
-    @Bean
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    public ExecutionPlanDependentWorkflowReconciler dependentWorkflowReconciler(KubernetesClient client) {
-        DeploymentDependentResource dependentResource = new DeploymentDependentResource();
-        dependentResource.setKubernetesClient(client);
-        dependentResource.configureWith(new KubernetesDependentResourceConfig()
-                .setLabelSelector(ExecutionPlanDependentWorkflowReconciler.DEPENDENT_RESOURCE_LABEL_SELECTOR));
-        return new ExecutionPlanDependentWorkflowReconciler(new WorkflowBuilder<ExecutionPlan>()
-                        .addDependentResource(dependentResource)
-                        .build(),
-                dependentResource);
     }
 
     @Bean(initMethod = "start", destroyMethod = "stop")
