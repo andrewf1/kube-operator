8227e8c1b45364edce488bae5e00a0cb780bb1d8
Andrew Fernandez <fdez26@ymail.com>
2023-08-29 19:16:21 -0400

    Changes CRD group name

--- src/main/java/af/ex/kube/kubeoperator/resource/custom/ExecutionPlan.java
+++ src/main/java/af/ex/kube/kubeoperator/resource/custom/ExecutionPlan.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-@Group("af.executionplan")
+@Group("ex.operator")
 @Version("v1")
 @Data
 @EqualsAndHashCode(callSuper = true)
